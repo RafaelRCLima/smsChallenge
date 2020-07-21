113bade8b76859332d6450bf5501079e0d677e69
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-07-21 18:14:33 -0300

    Created unit tests

---
+++ src/app/services/conversionService.js
@@ -0,0 +1,5 @@
+let conversionService = {
+  
+}
+
+module.exports = conversionService
---
+++ tests/unit/convertionService.test.js
@@ -0,0 +1,16 @@
+const conversionService = require('../../src/app/services/conversionService')
+
+describe('It should test conversionService functions', () => {
+  
+  test('Should convert a text to a number sequence', async () => {
+    const text = 'Teste de Mesa'
+    const response = await conversionService.convertTextToNumber(letters, text)
+    expect(response).toBe('833777783303_33063377772')
+  })
+
+  test('Should convert a number sequence to a text', async () => {
+    const numberSequence = '833777783303_33063377772'
+    const response = await conversionService.convertNumberToText(letters, numberSequence)
+    expect(response).toBe('teste de mesa')
+  })
+})
